0e765f4a6d7a2aa1b339d7a4c6803e4e8eb54b24
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-10-19 17:15:00 -0700

    Add split by lines

--- lib/fyorth.ex
+++ lib/fyorth.ex
@@ -15,4 +15,15 @@ defmodule Fyorth do
   def words(line) do
     line |> String.split
   end
+
+  @doc """
+  ## Examples
+
+      iex> Fyorth.lines(": 1 2 +\\n: 2 3 +")
+      [": 1 2 +", ": 2 3 +"]
+
+  """
+  def lines(context) do
+    context |> String.split("\n")
+  end
 end
